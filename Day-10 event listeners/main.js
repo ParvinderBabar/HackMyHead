96f63e74624c0ae1f4228b9c87587dc1658559f3
parvinderBabar <er.kaurparvinder@gmail.com>
2024-02-27 13:38:26 -0500

    commit

---
+++ Day-10 event listeners/main.js
@@ -0,0 +1,6 @@
+const btn_clickme = document.querySelector("#Clickme")
+btn_clickme.addEventListener("click", () =>
+{
+    const header = document.createElement("h1");
+    h1.textContent = "hello";
+})
